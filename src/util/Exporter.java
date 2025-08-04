cd75937baf50f057713b4806280de58ca678e890
JOE292 <yohanes.mesfin@bitscollege.edu.et>
2025-08-04 23:17:46 +0300

    Add student report formatting and export functionality

--- src/util/Exporter.java
+++ src/util/Exporter.java
@@ -36,4 +36,33 @@ public class Exporter {
     public static void exportRegistrations(List<Registration> regs) throws IOException {
         export(regs, "registrations.txt");
     }
+
+     public static void exportStudentWithCourses(Student student, List<Course> courses) throws IOException {
+        String exportDir = "exports";  
+        File dir = new File(exportDir);
+        if (!dir.exists()) dir.mkdirs();
+
+        String filename = "student_" + student.getStudentId() + ".txt";
+        File file = new File(dir, filename);
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write("Student ID: " + student.getStudentId());
+            writer.newLine();
+            writer.write("Name: " + student.getName());
+            writer.newLine();
+            writer.write("Email: " + student.getEmail());
+            writer.newLine();
+            writer.write("Major: " + student.getMajor());
+            writer.newLine();
+            writer.newLine();
+            writer.write("Registered Courses:");
+            writer.newLine();
+
+            for (Course c : courses) {
+                writer.write("- " + c.getTitle() + " | Instructor: " + c.getInstructor() + " | Credits: " + c.getCreditHours());
+                writer.newLine();
+            }
+        }
+    }
+
 }
